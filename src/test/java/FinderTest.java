f3a7bde07dccea8f6907e6e92b8fcff4d03e2e26
nasosvava <78432133+nasosvava@users.noreply.github.com>
2022-05-24 20:17:26 +0300

    S4DX

--- src/test/java/FinderTest.java
+++ src/test/java/FinderTest.java
@@ -2,8 +2,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.LinkedList;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,7 +14,10 @@ public class FinderTest {
 
     private static final  String[] thisIsAStringArray = {"asd","asdd","fre","glk","lkm","sad"};
 
-
+    private static final LinkedList<String> STRINGS = new LinkedList<>() {{
+        add("asd");
+        add("sad");
+    }};
     @Test
     @DisplayName("Should throw an Exception")
     void TestFindNullPointerException() {
@@ -23,19 +29,52 @@ public class FinderTest {
     }
 
     @Test
-    @DisplayName("Should find the char")
-    void TestFindSuccess() {
+    @DisplayName("Should find 2 values")
+    void TestTwoValursFoundSuccess() {
         Finder finder = new Finder(thisIsAStringArray);
-        assertEquals(2, finder.find("das").size());
+        assertEquals(2, finder.find("DAS").size());
     }
 
 
     @Test
-    @DisplayName("Should not find any char")
-    void TestFindNotFoundWord() {
+    @DisplayName("Should not find any values")
+    void TestNoValuesFoundWord() {
         Finder finder = new Finder(thisIsAStringArray);
         assertEquals(0, finder.find("LLL").size());
     }
 
+
+    @Test
+    @DisplayName("Should find exactly")
+    void TestFindValuesSuccess(){
+        Finder finder = new Finder(thisIsAStringArray);
+       assertEquals( STRINGS, finder.find("das"));
+    }
+
+    @Test
+    @DisplayName("Should not find any values")
+    void TestNoValuesFound(){
+        Finder finder = new Finder(thisIsAStringArray);
+        assertEquals( 0, finder.find("lll").size());
+    }
+    @Test
+    @DisplayName("This is performanse test")
+    void performanceTest(){
+        String[] strings = new String[9999999];
+
+            for (int i = 0; i < strings.length ; i++) {
+                byte[] array = new byte[3];
+                new Random().nextBytes(array);
+                String generatedString = new String(array, StandardCharsets.UTF_8);
+                strings[i] = generatedString;
+            }
+
+        assertTimeout(Duration.ofSeconds(1), () -> {
+
+            Finder finder = new Finder(strings);
+            finder.find("das");
+        });
+
+    }
 
 }
